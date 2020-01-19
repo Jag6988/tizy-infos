389b2db0c7d7468d6e2857841de541ae279301f0
Herbreteau <jgherbreteau@hotmail.com>
2020-01-19 14:41:51 +0100

    hey

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
   root to: 'pages#home'
-
+  get '/equipe', to: 'pages#equipe'
+  get '/industriel', to: 'pages#industriel'
+  get '/restaurateur', to: 'pages#restaurateur'
+  get '/distributeur', to: 'pages#distributeur'
+  get '/consommateur', to: 'pages#consommateur'
   resources :articles, only: %i[new create update edit destroy index show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
